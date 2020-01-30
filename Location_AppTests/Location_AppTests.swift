aedce1c63395b0f4235a03f139c9291592dab773
tushar40 <tushar@hotcocoasoftware.com>
2020-01-30 17:06:29 +0530

    Initial Commit

---
+++ Location_AppTests/Location_AppTests.swift
@@ -0,0 +1,34 @@
+//
+//  Location_AppTests.swift
+//  Location_AppTests
+//
+//  Created by Tushar Gusain on 30/01/20.
+//  Copyright © 2020 Hot Cocoa Software. All rights reserved.
+//
+
+import XCTest
+@testable import Location_App
+
+class Location_AppTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
